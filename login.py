b0363bb6b752c98cd53ab03d7e2ec0a64bf2f0cc
tangji08 <ji.tang@polytechnique.edu>
2020-03-04 19:19:47 +0100

    update index page

--- login.py
+++ login.py
@@ -8,7 +8,7 @@ def home():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        return 'Hello Mr.TANG!  <a href="/logout">Logout</a>'
+        return render_template('index.html')
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
